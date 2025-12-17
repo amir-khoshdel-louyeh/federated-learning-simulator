a127a1e9c95428e3fbe24b58d8003dce99beb9bb
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2025-12-17 10:44:20 +0100

    centeralized algorithm
    Co-authored-by: kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>

--- src/matlab/algorithms/centralized.m
+++ src/matlab/algorithms/centralized.m
@@ -1,6 +1,4 @@
-function acc = train_centralized(x_train, y_train, x_test, y_test)
-% Train centralized model for 3 epochs, batch size 64. Returns accuracy.
-    model = make_model();
-    model.fit(x_train, y_train, 'epochs', 3, 'batchSize', 64);
-    [~, acc] = model.evaluate(x_test, y_test);
+function acc = centralized(x_train, y_train, x_test, y_test)
+% Centralized training (wrapper), delegates to train_centralized helper.
+    acc = train_centralized(x_train, y_train, x_test, y_test);
 end
